2f783ca34ab8145edc741e3543ed58754aaab128
Luis Vegas <luisvegasmor@gmail.com>
2026-04-06 11:40:45 -0400

    feat: support for module nodes

--- maude-mode.el
+++ maude-mode.el
@@ -5,9 +5,12 @@
   '((t :foreground "#a855f7" :weight bold))
   "Purple color for types")
 
+(defface maude-module-face
+  '((t :foreground "#F52727" :weight bold))
+  "Red color for modules")
 
 (defface maude-attr-face
-  '((t :foreground "#FA0021" :weight bold))
+  '((t :foreground "#E82CC6" :weight bold))
   "color for attr")
 
 (defvar maude-ts-font-lock-rules
@@ -16,27 +19,43 @@
     ((keyword) @font-lock-keyword-face)
 
     :language maude
-    :feature identifier
-    ((identifier) @font-lock-type-face)
+    :feature keyword
+    ((module_type) @maude-module-face)
+
+    :language maude
+    :feature keyword
+    ((module_end) @maude-module-face)
 
     :language maude
     :feature op-attr
     ((op_attr) @maude-attr-face)
 
     :language maude
+    :feature identifier
+    ((identifier) @font-lock-variable-name-face)
+
+    :language maude
     :feature type
-    ((base_type) @maude-type-face)
-    ))
+    ((base_type) @maude-type-face)))
+
+(defvar maude-ts-indent-rules
+  '((maude
+     ((node-is "module_end") column-0 0)
+     ((node-is "module_type") column-0 0)
+     ((parent-is "module_entry") column-0 2)
+     (no-node column-0 0))))
 
 (defun maude-ts-setup ()
+  (setq-local tab-width 2)
+  (setq-local treesit-simple-indent-rules maude-ts-indent-rules)
   (setq-local treesit-font-lock-settings
               (apply #'treesit-font-lock-rules maude-ts-font-lock-rules))
-  (setq-local treesit-font-lock-feature-list '((keyword type op_attr identifier)))
+  (setq-local treesit-font-lock-feature-list '((keyword type op-attr identifier)))
   (treesit-major-mode-setup))
 
-(define-derived-mode maude-ts-mode prog-mode
-  "Maude"
+(define-derived-mode maude-ts-mode prog-mode "Maude"
   (when (treesit-ready-p 'maude)
+    (treesit-parser-create 'maude)
     (maude-ts-setup))
   (eglot-ensure))
 
@@ -44,14 +63,19 @@
 
 (defun maude-load-file ()
   (interactive)
-  (let ((file (buffer-file-name)))
+  (if (not (buffer-file-name))
+      (message "Buffer has no associated file")
+    (save-buffer)
     (maude-open-repl)
     (comint-send-string "*maude*"
-                        (format "in %s .\n" file))))
+                        (format "in %s .\n" (buffer-file-name)))))
 
-(defun maude-open-repl()
+(defun maude-open-repl ()
   (interactive)
-  (unless (get-buffer "*maude*")
+  (if (get-buffer "*maude")
+      (unless (get-buffer-process "*maude*")
+        (kill-buffer "*maude")
+        (make-comint "maude" "maude" nil "-interactive"))
     (make-comint "maude" "maude" nil "-interactive"))
   (switch-to-buffer-other-window "*maude*"))
 
